880804f9e272ae68c087ee67009a7514622e55e3
Jordan23-lab <76508670+Jordan23-lab@users.noreply.github.com>
2021-07-19 22:16:01 -0400

    Fix Search by Company Fucntion (#19)
    * Insert into Database function, Search based on role and company features
    
    * Fix Database and Insert/Search Methods
    
    * Fix review input types
    
    * Fix Search by Company Function
    
    * Fix Search by Company templating

---
+++ portfolio/src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -0,0 +1,182 @@
+package /*main.java.*/com.google.sps.servlets;
+
+
+import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.tofu.SoyTofu;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+import com.google.template.soy.tofu.SoyTofu;
+
+//import sun.java2d.pipe.Simple;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//package example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Search servlet implementation
+@WebServlet("/search")
+public class SearchServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L; // ensures that loaded class corresponds to a serialized object
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    try{
+    String company = request.getParameter("companyName");
+    
+    List<Review> reviews = selectBasedOnCompany(company);
+
+    
+
+    Review newReview = reviews.get(0);
+    System.out.println(newReview.review);
+
+
+    SoyFileSet sfs = SoyFileSet
+        .builder()
+        .add(Something.class.getResource("index.soy"))
+        .build();
+
+    // helloWorld
+    SoyTofu tofu = sfs.compileToTofu();
+    //System.out.println(
+        //tofu.newRenderer("com.google.sps.index.index").render());
+
+    // For convenience, create another SoyTofu object that has a
+    // namespace specified, so you can pass partial template names to
+    // the newRenderer() method.
+    SoyTofu simpleTofu = tofu.forNamespace("com.google.sps.index");
+
+
+
+    // helloName
+
+    List<Map<String,Object>> reviewsList = new ArrayList<Map<String, Object>>();
+
+    for (Review review : reviews) {
+        Map<String, Object> fields = new HashMap<String, Object>();
+        
+        fields.put("rating", review.rating);
+        fields.put("comment", review.review);
+        
+        reviewsList.add(fields);
+        
+    }
+
+
+    
+
+
+
+
+
+    Map<String, Object> data = new HashMap<>();
+    data.put("reviews", reviewsList);
+    data.put("companyName", newReview.company);
+    data.put("workBalance", newReview.wlb);
+    data.put("salary", newReview.salary);
+    //data.put("review", newReview.review);
+    data.put("role", newReview.role);
+    //data.put("ratings", ratingsList);
+    
+    System.out.println("-----------------");
+
+    //data.put("reviews", reviews);
+    
+   // data.put("starRatings", reviewsList);
+
+    //System.out.println(
+       // simpleTofu.newRenderer(".index").setData(data).render());
+
+  ((ServletResponse) response).setContentType("text/html");
+  response.setCharacterEncoding("utf-8");
+  try {
+    ((ServletResponse) response).getWriter().print(simpleTofu.newRenderer(".index").setData(data).render());
+    //((ServletResponse) response).getWriter().print(simpleTofu.newRenderer(".reviews").setData(data).render());
+  } catch (IOException e) {
+    //LOGGER.error("General IOException: {}", e.getMessage());
+  }  
+
+}catch (Exception e){
+    response.sendRedirect("index.html");
+
+}
+
+
+
+}
+
+   public List<Review> selectBasedOnCompany(String company){
+     try {
+       //creates instance of connection pool
+       DataSource connectionPool = DatabaseConnection.initializeDatabase();
+       List<Review> bookList = new ArrayList<>();
+       try (Connection conn = connectionPool.getConnection()) {
+           String stmt = String.format("SELECT * FROM reviews WHERE company = \"" + company + "\";");
+           try (PreparedStatement selectStmt = conn.prepareStatement(stmt)) {
+               selectStmt.setQueryTimeout(10); // 10s
+               ResultSet rs = selectStmt.executeQuery();
+               while (rs.next()) {
+                   //bookList.add(rs.getString(5));
+                   Review pendingReview = new Review (rs.getString(1), rs.getString(2), rs.getString(3),
+                   rs.getString(4), rs.getString(5), rs.getString(6));
+                   bookList.add(pendingReview);
+                   //bookList.add(rs.getString("review"));
+               }
+           }
+           }catch(SQLException e){
+                   System.out.println("failed");
+           }
+           System.out.println(bookList);
+           //System.out.println(bookList.get(0).salary);
+                   
+           
+           return bookList;
+ 
+       } catch (ClassNotFoundException | SQLException e) {
+           // TODO Auto-generated catch block
+           e.printStackTrace();
+       }
+        return null;
+ 
+ }
+
+
+}
+
+class Review{
+   String company, salary, rating, role, review, wlb;
+ 
+   public Review(String company, String salary, String rating,
+   String role, String review, String wlb){
+       this.company = company;
+       this.salary = salary;
+       this.rating = rating;
+       this.role = role;
+       this.review = review;
+       this.wlb = wlb;
+   }
+ 
+}
